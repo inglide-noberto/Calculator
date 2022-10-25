cd540ec4ada2e2e86e983ed7404072e015a85530
Inglide Noberto <inglidenoberto961@gmail.com>
2022-10-25 11:59:20 -0300

    changing variables to arrays

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,26 @@
+const btnNumbers = [];
+const btnOperations = [];
 const screen = document.querySelector('.screen-value');
-const reset = document.querySelector('.button-reset').textContent;
-const equal = document.querySelector('.buttons-equal').textContent;
-const point = document.getElementById('point').textContent;
-const btnSeven = document.getElementById('7').textContent;
-const btnEight = document.getElementById('8').textContent;
-const btnNine = document.getElementById('9').textContent;
-const btnFour = document.getElementById('4').textContent;
-const btnFive = document.getElementById('5').textContent;
-const btnSix = document.getElementById('6').textContent;
-const btnOne = document.getElementById('1').textContent;
-const btnTwo = document.getElementById('2').textContent;
-const btnThree = document.getElementById('3').textContent;
-const btnZero = document.getElementById('0').textContent;
-const btnDel = document.getElementById('del').textContent;
-const btnSum = document.getElementById('sum').textContent;
-const btnSub = document.getElementById('subtraction').textContent;
-const btnMult = document.getElementById('multiplication').textContent;
-const btnDiv = document.getElementById('division').textContent;
+
+btnNumbers.push( parseInt(document.getElementById('0').textContent));
+btnNumbers.push( parseInt(document.getElementById('1').textContent));
+btnNumbers.push( parseInt(document.getElementById('2').textContent));
+btnNumbers.push( parseInt(document.getElementById('3').textContent));
+btnNumbers.push( parseInt(document.getElementById('4').textContent));
+btnNumbers.push( parseInt(document.getElementById('5').textContent));
+btnNumbers.push( parseInt(document.getElementById('6').textContent));
+btnNumbers.push( parseInt(document.getElementById('7').textContent));
+btnNumbers.push( parseInt(document.getElementById('8').textContent));
+btnNumbers.push( parseInt(document.getElementById('9').textContent));
+
+btnOperations.push(document.getElementById('point').textContent);
+btnOperations.push(document.getElementById('sum').textContent);
+btnOperations.push(document.getElementById('subtraction').textContent);
+btnOperations.push(document.getElementById('multiplication').textContent);
+btnOperations.push(document.getElementById('division').textContent);
+btnOperations.push(document.getElementById('del').textContent);
+btnOperations.push(document.querySelector('.button-reset').textContent);
+btnOperations.push(document.querySelector('.buttons-equal').textContent);
+
+console.log( btnNumbers);
+console.log( btnOperations);
