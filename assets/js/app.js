a568b21479bdaff85424de29d7a46f024f00ed93
Inglide Noberto <inglidenoberto961@gmail.com>
2022-10-25 22:15:24 -0300

    redefining call logic
    calling elements on calculator screen

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,19 @@
-const btnNumbers = [];
-const btnOperations = [];
+const selector = document.querySelectorAll('.selector');
 const screen = document.querySelector('.screen-value');
 
-btnNumbers.push( parseInt(document.getElementById('0').textContent));
-btnNumbers.push( parseInt(document.getElementById('1').textContent));
-btnNumbers.push( parseInt(document.getElementById('2').textContent));
-btnNumbers.push( parseInt(document.getElementById('3').textContent));
-btnNumbers.push( parseInt(document.getElementById('4').textContent));
-btnNumbers.push( parseInt(document.getElementById('5').textContent));
-btnNumbers.push( parseInt(document.getElementById('6').textContent));
-btnNumbers.push( parseInt(document.getElementById('7').textContent));
-btnNumbers.push( parseInt(document.getElementById('8').textContent));
-btnNumbers.push( parseInt(document.getElementById('9').textContent));
+const btnOperations = [];
+const btnNumbers = [];
+// Alterei as variáveis para arrays pois seria melhor de manusear devido a quantidade de botoes 
+btnNumbers.push(document.getElementById('0').textContent);
+btnNumbers.push(document.getElementById('1').textContent);
+btnNumbers.push(document.getElementById('2').textContent);
+btnNumbers.push(document.getElementById('3').textContent);
+btnNumbers.push(document.getElementById('4').textContent);
+btnNumbers.push(document.getElementById('5').textContent);
+btnNumbers.push(document.getElementById('6').textContent);
+btnNumbers.push(document.getElementById('7').textContent);
+btnNumbers.push(document.getElementById('8').textContent);
+btnNumbers.push(document.getElementById('9').textContent);
 
 btnOperations.push(document.getElementById('point').textContent);
 btnOperations.push(document.getElementById('sum').textContent);
@@ -22,5 +24,17 @@ btnOperations.push(document.getElementById('del').textContent);
 btnOperations.push(document.querySelector('.button-reset').textContent);
 btnOperations.push(document.querySelector('.buttons-equal').textContent);
 
-console.log( btnNumbers);
-console.log( btnOperations);
+console.log(btnNumbers);
+console.log(btnOperations);
+
+selector.forEach(element =>{
+    element.addEventListener('click', (e)=>{
+        if (e.target.matches('button')) {
+            if (screen.textContent === '0') {
+                screen.textContent = e.target.textContent;
+            }else{
+                screen.textContent = screen.textContent + e.target.textContent;
+            }
+        }
+    })
+})
