1588dc9f04ab7c74dcd19d8b58c94913a857e725
Inglide Noberto <inglidenoberto961@gmail.com>
2022-10-27 22:01:14 -0300

    calculator logic finished
    calculator logic v1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,40 @@
 const selector = document.querySelectorAll('.selector');
 const screen = document.querySelector('.screen-value');
 
-let press = false;
-
+let pressed = false;
+let value1;
+let op;
 selector.forEach(element => {
     element.addEventListener('click', (e) => {
         const buttonValue = e.target;
         const action = buttonValue.dataset.operation;
+        
+        const result = (n1, n2, op) => {
+            let res = '';
+            switch (op) {
+                case 'sum':
+                    res = parseFloat(n1) + parseFloat(n2);
+                    break;
+                case 'subtraction':
+                    res = parseFloat(n1) - parseFloat(n2);
+                    break;
+                case 'multiplication':
+                    res = parseFloat(n1) * parseFloat(n2);
+                    break;
+                case 'division':
+                    res = parseFloat(n1) / parseFloat(n2);
+                    break;
+                default:
+                    console.log(err);
+            }
+            return res;
+        }
         if (buttonValue.matches('button') && !action) {
-            if (screen.textContent === '0' || press === true) {
+            if (screen.textContent === '0' || pressed === true) {
                 screen.textContent = buttonValue.textContent;
-                press ? buttonValue.removeAttribute('disabled') : console.log("desabilitado")
-                console.log(press)
-            } else {
+                pressed = false;
+            }else {
                 screen.textContent = screen.textContent + buttonValue.textContent;
-                console.log(press)
             }
         } else if (
             action === 'sum' ||
@@ -22,36 +42,20 @@ selector.forEach(element => {
             action === 'division' ||
             action === 'multiplication'
         ) {
-            const value1 =  parseInt(screen.textContent);
-            buttonValue.setAttribute('disabled', '');
-            press = true;
-        } else {
-            switch (action) {
-                case 'point':
-                    screen.textContent = screen.textContent + ",";
-                    break;
-                case 'del':
-                    screen.textContent = "0";
-                    break;
-                case 'reset':
-                    screen.textContent = "0";
-                    break;
-                case 'sum':
-                    screen.textContent = "0";
-                    break;
-                case 'subtraction':
-                    screen.textContent = "0";
-                    break;
-                case 'division':
-                    screen.textContent = "0";
-                    break;
-                case 'multiplication':
-                    screen.textContent = "0";
-                    break;
-                case 'equals':
-                    screen.textContent = "0";
-                    break;
-            }
+            value1 = screen.textContent;
+            op = action;
+            pressed = true;
+        } else if (action === 'point') {
+                screen.textContent = screen.textContent + '.';
+        } else if (action === 'equals') {
+            console.log(value1);
+            const value2 = screen.textContent;
+            console.log(value2);
+            console.log(op);
+            screen.textContent = result(value1, value2, op)
+        }else if (action === 'reset' || action === 'del') {
+            screen.textContent = '';
+            screen.textContent = '0';
         }
     })
 })
